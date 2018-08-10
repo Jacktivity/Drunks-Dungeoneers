74577fd1dfd2c5711dd099d239ea52fa3ff644ce
Jacktivity <jh140695@gmail.com>
2018-08-10 21:09:56 +0100

    Added player and drink template

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    private int health;
+    private float hSpeed;
+    private float vSpeed;
+
+    private DrinkTemplate drink;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
