82b38e3399c2079faf5c0ce1a4bf75bf4623bd5c
JohnShone94 <jshone94@yahoo.com>
2018-08-11 02:43:32 +0100

    Update 0.0.2

---
+++ Assets/Scripts/UI/SettingsClass.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+public class SettingsClass
+{
+    //John Shone - 10/08/2018
+    /**
+    This class is used to store the constants.
+    We then save these constants to a file to load later.
+    **/
+    //Public Variables
+    public bool fullscreen;
+    public int resolutionInd;
+    public int qualityLevel;
+    public float musicVolume;
+}
