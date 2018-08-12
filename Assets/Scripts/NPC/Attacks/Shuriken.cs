2783f1f4cad0e7f30a6bf262dbd1a638aa447867
FinalFreedom <u1664298@unimail.hud.ac.uk>
2018-08-12 15:10:42 +0100

    Meh

---
+++ Assets/Scripts/NPC/Attacks/Shuriken.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shuriken : Attack {
+    private int bounces;
+    protected override void Miss(Collision collision)
+    {
+        base.Miss(collision);
+        bounces++;
+        if(bounces >= 4)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            float x = transform.position.x - collision.gameObject.transform.position.x;
+            if(x >= 0)
+            {
+
+            }
+            
+        }
+    }
+}
