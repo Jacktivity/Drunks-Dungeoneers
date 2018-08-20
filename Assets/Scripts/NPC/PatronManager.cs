48b319b0d32224746974e50d462dc0d5e8a60294
JohnShone94 <jshone94@yahoo.com>
2018-08-20 23:34:29 +0100

    Changeing of the patron manager

--- Assets/Scripts/NPC/PatronManager.cs
+++ Assets/Scripts/NPC/PatronManager.cs
@@ -26,19 +26,12 @@ public class PatronManager : MonoBehaviour {
         //If we can get a path to the destination
         if (destination != null)
         {
-<<<<<<< HEAD
-
 
             //Patron.Race randRace = (Patron.Race)Random.Range(0, 4);
            //Patron.Class randClass = (Patron.Class)Random.Range(0, 4);
 
             Patron.Race randRace = (Patron.Race)Random.Range(0, heads.Length);
             Patron.Class randClass = (Patron.Class)Random.Range(0, bodies.Length);
-
-=======
-            Patron.Race randRace = (Patron.Race)Random.Range(0, heads.Length);
-            Patron.Class randClass = (Patron.Class)Random.Range(0, bodies.Length);
->>>>>>> master
 
             Sprite head = heads[(int)randClass];
             Sprite body = bodies[(int)randRace];
