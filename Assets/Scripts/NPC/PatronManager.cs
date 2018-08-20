5b841882e4a2daaf4c07c618775ebf9d8a87bca5
JohnShone94 <jshone94@yahoo.com>
2018-08-20 22:01:32 +0100

    Changes to Patron Manager

--- Assets/Scripts/NPC/PatronManager.cs
+++ Assets/Scripts/NPC/PatronManager.cs
@@ -28,13 +28,13 @@ public class PatronManager : MonoBehaviour {
         if (destination != null)
         {
 
-<<<<<<< HEAD
-            Patron.Race randRace = (Patron.Race)Random.Range(0, 4);
-            Patron.Class randClass = (Patron.Class)Random.Range(0, 4);
-=======
+
+            //Patron.Race randRace = (Patron.Race)Random.Range(0, 4);
+           //Patron.Class randClass = (Patron.Class)Random.Range(0, 4);
+
             Patron.Race randRace = (Patron.Race)Random.Range(0, heads.Length);
             Patron.Class randClass = (Patron.Class)Random.Range(0, bodies.Length);
->>>>>>> Environments
+
 
             Sprite head = heads[(int)randClass];
             Sprite body = bodies[(int)randRace];
